225eed220c812aa450f9f15d5839cf333e3c13fa
aswindevsp <aswindevsp@gmail.com>
2022-12-11 12:24:08 +0530

    clean up

---
+++ app/src/main/java/com/mgits/complaintreg/ui/auth/reset_password/ResetPassword.kt
@@ -0,0 +1,23 @@
+package com.mgits.complaintreg.ui.auth.reset_password
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import com.mgits.complaintreg.ui.auth.login.LoginViewModel
+
+@Composable
+fun ResetPassword(viewModel: LoginViewModel, navController: NavController) {
+
+
+}
+
+
