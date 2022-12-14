e3a225ab1d6644bedcc8b697e735392f766fc581
aswindevsp <aswindevsp@gmail.com>
2022-12-14 22:57:13 +0530

    logout

--- app/src/main/java/com/mgits/complaintreg/ui/home/user/UserDrawer.kt
+++ app/src/main/java/com/mgits/complaintreg/ui/home/user/UserDrawer.kt
@@ -1,7 +1,21 @@
 package com.mgits.complaintreg.ui.home.user
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.mgits.complaintreg.navigation.ROUTE_LOGIN
+import com.mgits.complaintreg.navigation.ROUTE_USER_HOME
 
-class UserDrawer(navController: NavController) {
-
+@Composable
+fun UserDrawer(navController: NavController) {
+    Button(onClick =
+    {
+        Firebase.auth.signOut()
+        navController.navigate(ROUTE_LOGIN)
+    }) {
+        Text(text = "LogOut");
+    }
 }
