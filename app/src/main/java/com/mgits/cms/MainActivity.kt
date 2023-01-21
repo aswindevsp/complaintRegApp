1333f0f43805a5e29b2d3b3321f9f6e9a73130c7
aswindevsp <aswindevsp@gmail.com>
2023-01-22 00:15:23 +0530

    bumped gradle to 7.4
    Signed-off-by: aswindevsp <aswindevsp@gmail.com>

--- app/src/main/java/com/mgits/cms/MainActivity.kt
+++ app/src/main/java/com/mgits/cms/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.mgits.cms
 
-import android.content.ContentValues.TAG
+
 import android.os.Build
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Email
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import com.google.firebase.auth.EmailAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.mgits.cms.navigation.AppNavHost
 import com.mgits.cms.ui.theme.cmsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
